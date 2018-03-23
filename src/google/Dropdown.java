655c0a33141939ddb60d0041c4ce7ff43586e125
nagarjuna <nagarjuna@Nagarjuna-Y.citcert.com>
2018-03-23 17:58:22 +0530

    Fourth commit with screenshot file

--- src/google/Dropdown.java
+++ src/google/Dropdown.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class Dropdown 
 {
 	
-   private WebDriver driver;
+    private WebDriver driver;
 	
 	@BeforeMethod
 	public void Startup_Openbrowser()
---
+++ src/google/Screenshot.java
@@ -0,0 +1,59 @@
+package google;
+
+
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+
+
+
+
+
+
+public class Screenshot 
+{
+	WebDriver driver;
+	@BeforeMethod
+	public void Startup_Openbrowser()
+	{
+        /*Initiate driver*/
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "E:\\Selenium\\geckodriver-v0.20.0-win64\\geckodriver.exe");
+		driver= new ChromeDriver();
+		//driver= new FirefoxDriver();
+		driver.get("https://www.google.co.in");
+		//driver.get("http://rakurakuforpc-env.us-west-1.elasticbeanstalk.com/");
+		driver.manage().window().maximize();
+     }
+	
+	@Test
+	public void Screenshots() throws InterruptedException, Exception
+	{      
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File src=ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("./Screenshots/img.png"));
+		System.out.println("Screenshot captured");
+		
+    }
+	
+	
+	/*@AfterMethod
+	public void Closebrowser() 
+	{
+		driver.close();
+	}*/
+
+}
