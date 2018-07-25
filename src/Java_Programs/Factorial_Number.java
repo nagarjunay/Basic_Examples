f8eac39a75d42b4015bed4ad99067e2febfdd698
nagarjuna <nagarjuna@Nagarjuna-Y.citcert.com>
2018-07-25 11:04:46 +0530

    commit

--- src/Java_Programs/Factorial_Number.java
+++ src/Java_Programs/Factorial_Number.java
@@ -2,6 +2,7 @@ package Java_Programs;
 
 public class Factorial_Number {
 	
+	//the factorial Program using loop in java.
 	 public static void main(String args[]){  
 		  int i,fact=1;  
 		  int number=5;//It is the number to calculate factorial    
