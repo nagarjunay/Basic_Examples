9508c575445c90f52e51a09b66bb3d885b922da3
nagarjuna <nagarjuna@Nagarjuna-Y.citcert.com>
2018-07-25 11:03:38 +0530

    commit

---
+++ src/Java_Programs/Factorial_Number.java
@@ -0,0 +1,32 @@
+package Java_Programs;
+
+public class Factorial_Number {
+	
+	 public static void main(String args[]){  
+		  int i,fact=1;  
+		  int number=5;//It is the number to calculate factorial    
+		  for(i=1;i<=number;i++){    
+		      fact=fact*i;    
+		  }    
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }
+	 
+	 
+	 
+	// the factorial program in java using recursion.
+	 
+	 class FactorialExample2{  
+		 static int factorial(int n){    
+		  if (n == 0)    
+		    return 1;    
+		  else    
+		    return(n * factorial(n-1));    
+		 }    
+		 public static void main(String args[]){  
+		  int i,fact=1;  
+		  int number=4;//It is the number to calculate factorial    
+		  fact = factorial(number);   
+		  System.out.println("Factorial of "+number+" is: "+fact);    
+		 }  
+
+}
