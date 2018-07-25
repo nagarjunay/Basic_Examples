fccef3983aac8dd6b0c0c045d193483bfbb2e5dd
nagarjuna <nagarjuna@Nagarjuna-Y.citcert.com>
2018-07-25 17:51:45 +0530

    commit

---
+++ src/google/DynamicGoogleSearch.java
@@ -0,0 +1,38 @@
+package google;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DynamicGoogleSearch {
+	WebDriver driver;
+	@BeforeTest
+	public void setup() throws Exception {
+		System.setProperty("webdriver.chrome.driver",
+				"D:\\E drive\\Selenium\\chromedriver_win32\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		// driver= new RemoteWebDriver(new
+		// URL("http://localhost:4444/wd/hub"),DesiredCapabilities.chrome());
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+
+}
+	
+	@Test
+	public void Google() {
+		
+		driver.findElement(By.id("lst-ib")).sendKeys("test");
+		List<WebElement> list = driver.findElements(By.xpath("//ul[@role='listbox']//li/descendant::div[@class='sbqs_c']"));
+		System.out.println("Total of suggestions in google search box" + list.size());
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i).getText());
+		}
+	}
+}
