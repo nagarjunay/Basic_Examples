dd2e8f4825662d58c6dc7c3145303b56ad6e33dc
nagarjuna <nagarjuna@Nagarjuna-Y.citcert.com>
2018-08-06 11:47:57 +0530

    commit

---
+++ src/Java_Programs/Reverse_String.java
@@ -0,0 +1,16 @@
+package Java_Programs;
+
+public class Reverse_String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Hello Java"; 
+
+		for (int i = str.length() - 1; i >= 0; i--) { 
+		System.out.print(str.charAt(i)); 
+		} 
+
+	}
+
+}
